b586dfcd340ab062c704c9d7d01e500c24f792a1
ZFYoung <youngya@gmail.com>
2017-12-04 23:20:41 +0800

    new register and removeaccount page

--- authenize.js
+++ authenize.js
@@ -1,30 +1,36 @@
 var usermanager = require('./usermanager')
 var http = require('http')
 var querystring = require('querystring')
-var url = require('url')
+var url = require('url');
+var fs = require('fs');
+var path = require('path');
 
 var server = http.createServer(function(req,res){
 
 	var pathname = url.parse(req.url).pathname;
 
-	if (pathname == '/postlogin') {
-		var postData = '';
-		req.on('data',function(chunk){
-			postData += chunk;
-		});
+	var postData = '';
+	req.on('data',function(chunk){
+		postData += chunk;
+	});
+	
+	req.on('end',function(){
+		console.log('receive path====== ' + pathname + ' ======data ===========' +postData);
 		
-		req.on('end',function(){
-			console.log('receive path====== ' + pathname + ' ======data ===========' +postData);
-			
-			var requser = querystring.parse(postData);
-			if(postData && postData.indexOf('&') < 0) {
-				requser = JSON.parse(postData);
-			} else {
-				requser = querystring.parse(postData);
-			}
 
-			if (requser.uname.length > 0 && requser.upwd.length > 0) {
-				usermanager.checkUser(requser.uname,requser.upwd,function(result, respUser){
+		var postDataModel = '';
+		if (req.method == "GET") {
+			var getquery = url.parse(req.url).query;
+			postDataModel = querystring.parse(getquery);
+		}
+		else if(postData && postData.indexOf('&') < 0) {
+			postDataModel = JSON.parse(postData);
+		}
+
+
+		if (pathname == '/postlogin') {
+			if (postDataModel.uname.length > 0 && postDataModel.upwd.length > 0) {
+				usermanager.checkUser(postDataModel.uname,postDataModel.upwd,function(result, respUser){
 					console.log('auth result ===========' + result);
 					if (result === true) {
 						res.writeHead(200,{'Content-Type':'text/plain'});
@@ -41,14 +47,51 @@ var server = http.createServer(function(req,res){
 				res.write(JSON.stringify({'retCode':'0','USER':""}));
 				res.end();
 			}
-		});
-	} else if(pathname == '/getallusers') {
-		usermanager.getAllUsers(function(allusers){
-			res.writeHead(200,{'Content-Type':'text/plain'});
-			res.write(JSON.stringify({'retCode':'1',"ALLUSERS":JSON.stringify(allusers)}));
-			res.end();
-		})
-	}
+		} else if(pathname == '/getallusers') {
+			usermanager.getAllUsers(function(allusers){
+				res.writeHead(200,{'Content-Type':'text/plain'});
+				res.write(JSON.stringify({'retCode':'1',"ALLUSERS":JSON.stringify(allusers)}));
+				res.end();
+			})
+		} else if(pathname == '/postregister') {
+			usermanager.createNewUser(postDataModel.username,postDataModel.userpwd,postDataModel.useravatar,postDataModel.useroneword,function(result, msg){
+				res.writeHead(200,{'Content-Type':'text/plain'});
+				res.write(JSON.stringify({'retCode':'0','message':msg}));
+				res.end();		
+			})
+		} else if(pathname == 'getdeleteuser') {
+			if (postDataModel.uname.length > 0 && postDataModel.upwd.length > 0) {
+				usermanager.deleteUser(postDataModel.uname,postDataModel.upwd,function(result){
+					if (result === true) {
+						res.writeHead(200,{'Content-Type':'text/plain'});
+						res.write('删除成功');
+						res.end();
+					} else {
+						res.writeHead(200,{'Content-Type':'text/plain'});
+						res.write('删除失败');
+						res.end();
+					}
+				})
+			} else {
+				res.writeHead(200,{'Content-Type':'text/plain'});
+				res.write('删除失败，账号密码有误！');
+				res.end();
+			}
+		}
+		else {
+			var realpath = path.join(__dirname,'WWW',pathname)
+			fs.readFile(realpath, 'binary', function(err, file) {
+				if (err) {
+					res.writeHead(500,{'Content-Type':'text/plain'});
+					res.end();
+				} else {
+					res.writeHead(200,{'Content-Type':'text/html'});
+					res.write(file, 'binary');
+					res.end();
+				}
+			})
+		}
+	});
 }).listen(3000);
 
 
--- usermanager.js
+++ usermanager.js
@@ -2,10 +2,11 @@ var fs = require('fs')
 var path = require('path')
 var mongo = require('./mongoManager')
 
-var usrdbpath = path.join(__dirname,'testdata/testuser.txt')
+var collection = 'usertb'
+
 var getAllUsers = function(callback){
 
-	mongo.selectData('usertb', function(result){
+	mongo.selectData(collection, function(result){
 		callback(result);
 	})
 }
@@ -13,7 +14,7 @@ var getAllUsers = function(callback){
 var checkUser = function(uname, upwd, callback){
 
 	var condition = {'username':uname, 'userpwd':upwd};
-	mongo.selectDataWithCondition('usertb', condition, function(result) {
+	mongo.selectDataWithCondition(collection, condition, function(result) {
 		if (result.length > 0) {
 			callback(true, result[0])
 		} else {
@@ -25,11 +26,40 @@ var checkUser = function(uname, upwd, callback){
 var getUserWithId = function(uid,callback){
 
 	var condition = {'_id':uid};
-	mongo.selectDataWithCondition('usertb', condition, function(result) {
+	mongo.selectDataWithCondition(collection, condition, function(result) {
 		callback(result);
 	})
 }
 
+var createNewUser = function(uname, upwd, uavatar, uoneword, callback) {
+	checkUser(uname, upwd, function(result,users) {
+		console.log(users)
+		if (!users) {
+			var dataUser = {"username":uname, "userpwd":upwd, "useravatar":uavatar, "useroneword":uoneword}
+			mongo.insertData(collection, dataUser, function(result){
+				console.log('插入成功')
+				callback(true,'成功');
+			})
+		} else {
+			console.log('插入失败')
+			callback(false,'用户已存在');
+		}
+	})
+}
+
+var deleteUser = function(uname, upwd, callback){
+
+	var condition = {'username':uname, 'userpwd':upwd};
+	mongo.deleteData(collection, condition, function(result) {
+		if (result.length > 0) {
+			callback(true,)
+		} else {
+			callback(false)
+		}
+	})
+};
+
 exports.checkUser = checkUser;
 exports.getUserWithId = getUserWithId;
 exports.getAllUsers = getAllUsers;
+exports.createNewUser = createNewUser;
